3c1ab168c152e78afa9e7202ec8aa44fb3532bdc
kerimovmurat <kerimovmurat@yandex.ru>
2024-12-08 13:58:32 +0300

    Update modul16_3.py

--- modul16_3.py
+++ modul16_3.py
@@ -9,28 +9,35 @@ users = {'1': 'Имя: Example, возраст: 18'}
 @app.get("/")
 async def start_page() -> dict:
     return {"message": "Главная страница"}
+
     # 4 CRUD запроса:
 @app.get('/users')
 async def get_message() -> dict:
     return users
 
 @app.post('/user/{username}/{age}')
-async def create_user(username: str, age: int) -> str:
+async def create_user(username:str = Path(min_length=5, max_length=20, description='Enter username', example='UrbanUser'),
+                      age:int = Path(ge=18, le=120, description='Enter age', example='77')) -> dict:
     user_id = str(int(max(users, key=int)) + 1)
     users[user_id] = f"Имя: {username}, возраст: {age}"
     return f"User {user_id} is registered"
 
 @app.put('/user/{user_id}/{username}/{age}')
-async def update_user(user_id: str, username: str, age: int) -> str:
+async def update_user(username: Annotated[str, Path(min_length=5, max_length=20, description="Enter username",
+                                                     example="UrbanUser")],
+                       user_id: str = Path(ge=1, le=100, description="Enter User ID", example="25"),
+                       age: int = Path(ge=18, le=120, description="Enter age", example="24")) -> str:
     if user_id not in users:
         raise HTTPException(status_code=404, detail="User not found")
     users[user_id] = f"Имя: {username}, возраст: {age}"
     return f"The user {user_id} is updated"
 
 @app.delete('/user/{user_id}')
-async def del_user(user_id: str) -> str:
+async def del_user(user_id: str = Path(ge=1, le=100, description="Enter User ID", example="25"),) -> str:
     if user_id not in users:
         raise HTTPException(status_code=404, detail="User not found")
     users.pop(user_id)
     return f"User {user_id} was deleted."
+
+
 
